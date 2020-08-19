8bed49639975293eae5bf3ca06e96f54eae0590e
ljl979 <2248491874@qq.com>
2020-08-19 15:00:05 +0800

    添加hello方法

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.jk.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author : ljl
  * @date : 14:44 2020/8/19
  * @user : 86187
  */
 public class UserController {
+    @RequestMapping("/hello")
+    public void hello(){
+
+    }
 }
